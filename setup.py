443d3f6424b3ee7e51e0c73e5eaaeec1debdb3a5
Heiner Tholen <github@heinertholen.com>
2014-04-30 00:38:02 +0200

    version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name='pylibconfig2',
     description="Pure python library for libconfig syntax.",
-    version='0.1.1',
+    version='0.1.2',
     author="Heiner Tholen",
     author_email="github@heinertholen.com",
     url="https://github.com/heinzK1X/pylibconfig2",
