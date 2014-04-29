3ffa1ed3c48b61ee67036a1e6b6ed718500ff6cb
Heiner Tholen <github@heinertholen.com>
2014-04-29 14:43:11 +0200

    nice imports in __init__

--- pylibconfig2/__init__.py
+++ pylibconfig2/__init__.py
@@ -1,1 +1,11 @@
-__author__ = 'heinzAdmin'
+from conf_types import \
+    Config, \
+    ConfError, \
+    ConfGroup, \
+    ConfList, \
+    ConfArray
+from parsing import \
+    ParseException, \
+    ParseFatalException
+
+PyLibConfigErrors = (ConfError, ParseException, ParseFatalException)
